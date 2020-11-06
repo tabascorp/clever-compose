c312373080361c336d35ffaa7d083387a562dade
szymon.werkowski <szymon.werkowski@best.wroclaw.pl>
2020-11-06 14:04:45 +0100

    PairCoding with @bzagozda; Cleaning up code;

--- src/logic/validators.ts
+++ src/logic/validators.ts
@@ -1,10 +1,11 @@
-export const validateInt = (value: string): (boolean | string) => {
-  if (parseFloat(value) <= 0) return 'You need to have at least one of them';
-  return Number.isInteger(parseFloat(value)) || 'Please enter an int number';
-}
-
 type Validator = (value: string) => (boolean | string)
 
-export const validateIndexOf = (versions: string[], errorMessage: string): Validator => {
-  return (value: string) => versions.indexOf(value) !== -1 || errorMessage
+export const validateInt: Validator = (value) => {
+  const parsedValue = parseFloat(value);
+  if (parsedValue <= 0) return 'You need to have at least one of them';
+  return Number.isInteger(parsedValue) || 'Please enter an int number';
+}
+
+export const validateIndexOf = (arr: string[], errorMessage: string): Validator => {
+  return (value: string) => arr.indexOf(value) !== -1 || errorMessage
 }
