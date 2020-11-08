0d0e1e6ddad2232d97d01df15fe62e0989157a4e
Bartek <bzagozda@hotmail.com>
2020-11-08 20:37:06 +0100

    Ask about template files path if settings not exist

--- src/logic/data/userData.ts
+++ src/logic/data/userData.ts
@@ -2,25 +2,45 @@ import * as fs from 'fs';
 import * as os from 'os';
 import * as path from 'path';
 
+import { prompt } from 'inquirer';
+
 const configBaseDir = path.join(os.homedir(), '.clever-compose');
 const configFileName = 'settings.json';
 const configPath = path.join(configBaseDir, configFileName);
 const defaultTemplatesPath = path.join(os.homedir(), 'clever-compose', 'templates');
 
+let templatesPath;
+
 const createSettingsContent = () => ({
-  templatesPath: defaultTemplatesPath,
+  templatesPath,
 });
 
-export function loadUserData(): Promise<void> {
+async function askForTemplatePath(): Promise<string> {
+  const result = await prompt([{
+    type: 'input',
+    name: 'templatesPath',
+    message: 'Set path for your templates',
+    default: defaultTemplatesPath,
+  }]);
+  return result.templatesPath;
+}
+
+export async function loadUserData(): Promise<void> {
   if (fs.existsSync(configPath)) {
-    return Promise.resolve();
+    return fs.promises.readFile(configPath, { encoding: 'utf8' })
+      .then((fileBuffer) => {
+        templatesPath = JSON.parse(fileBuffer).templatesPath;
+        console.log(templatesPath);
+      });
   }
 
-  return fs.promises.mkdir((configBaseDir), { recursive: true })
-    .then(() => fs.promises.writeFile(configPath, JSON.stringify(createSettingsContent())))
+  templatesPath = await askForTemplatePath();
+
+  await fs.promises.mkdir((configBaseDir), { recursive: true });
+  return fs.promises.writeFile(configPath, JSON.stringify(createSettingsContent()))
     .catch(console.error);
 }
 
 export default {
-  templatesPath: undefined,
+  templatesPath,
 };
