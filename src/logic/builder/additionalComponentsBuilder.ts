19da792d5286b7a3eff46c309df783226062555b
Bartek <bzagozda@hotmail.com>
2020-11-12 14:17:26 +0100

    Add testing framework and few tests

--- src/logic/builder/additionalComponentsBuilder.ts
+++ src/logic/builder/additionalComponentsBuilder.ts
@@ -1,11 +1,36 @@
 import ph from './constants';
 
-export default (serviceInfo: Record<string, any>): Record<string, any> => {
-  const addons = {};
+function parseServiceInfoNumbers(serviceInfo: Record<string, any>): Record<string, any> {
+  const result = {};
 
-  if (serviceInfo.additionalComponents) {
-    serviceInfo.additionalComponents
-      .forEach((item: string) => { addons[item] = new Array(parseInt(serviceInfo[`${item}-quantity`], 10)).fill(`${ph}:`); });
+  Object.keys(serviceInfo).forEach((key) => {
+    if (key.endsWith('-quantity')) {
+      result[key] = parseInt(serviceInfo[key], 10);
+    } else {
+      result[key] = serviceInfo[key];
+    }
+  });
+
+  return result;
+}
+
+const getItemQuantity = (serviceInfo: Record<string, any>, item: string): number => serviceInfo[`${item}-quantity`];
+
+const processAdditionalComponents = (serviceInfo) => serviceInfo.additionalComponents
+  .filter((item: string) => getItemQuantity(serviceInfo, item) > 0)
+  .reduce((obj: Record<string, any>, item: string) => {
+    const newObj = obj;
+    newObj[item] = new Array(getItemQuantity(serviceInfo, item)).fill(`${ph}:`);
+    return newObj;
+  }, {});
+
+export default (serviceInfo: Record<string, any>): Record<string, any> => {
+  let addons = {};
+
+  const parsedServiceInfo = parseServiceInfoNumbers(serviceInfo);
+
+  if (parsedServiceInfo.additionalComponents) {
+    addons = processAdditionalComponents(parsedServiceInfo);
   }
 
   return addons;
