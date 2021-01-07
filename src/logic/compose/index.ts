b1273d48642a69e29ff13f6a6743c502a32b1c8c
szymon.werkowski <szymon.werkowski@best.wroclaw.pl>
2021-01-08 00:04:11 +0100

    Await in inquirer works, and it's super easy...;

--- src/logic/compose/index.ts
+++ src/logic/compose/index.ts
@@ -3,18 +3,14 @@ import { serviceQuestions } from '../questions';
 
 export default async (composeInformations: Record<string, any>) => {
   const quantity = parseInt(composeInformations['services-quantity'], 10);
-
-  let promptQuestions = new Promise(() => { serviceParams: [] });
+  let serviceParams = [];
 
   for(let count = 1; count <= quantity; count += 1) {
     console.log(`\n Set information about ${count} service`);
-    promptQuestions = promptQuestions.then(async ({serviceParams}) => {
-      return prompt(serviceQuestions).then((response) => { 
-        serviceParams.push(response)
-        return {serviceParams}
-      });
-    });
+
+    const answers = await prompt(serviceQuestions);
+    serviceParams.push(answers);
   }
 
-  return promptQuestions.then(({serviceParams}) => ({serviceParams, composeInformations}));
+  return {serviceParams, composeInformations};
 };
