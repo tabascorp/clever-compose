5e46e31d5d193071c58a74f1f4c859a698cd162d
Bartosz Zagozda <bzagozda@valuelogic.one>
2021-01-17 15:01:15 +0100

    Fix missing build files on npm repository

--- src/logic/service/index.ts
+++ src/logic/service/index.ts
@@ -1,21 +1,5 @@
-// import { Component } from '../serviceComponents/default';
-
 export type Service = Record<string, any>;
 
 export type Services = Record<string, Service>;
 
 export type ServiceData = Record<string, any>;
-
-// class RealService {
-//   constructor(
-//     readonly build?: Component,
-//     readonly cap_add?: string[],
-//     readonly cap_drop?: string[],
-//     readonly cgroup_parent?: string,
-//     readonly command?: string | string[],
-//     readonly container_name?,
-//     readonly image?,
-//     readonly restart?,
-//     readonly deploy?,
-//   ) {}
-// }
